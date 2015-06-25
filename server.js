55c245e8a4761d20ded35b8dc174f9ef66a2b41c
ferrarima <yzxz@hotmail.com>
2015-06-25 12:47:26 +0800

    try express

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ var port = 18080;
 http.createServer(function (req, res){
 	res.writeHead(200, {'Content-type':'text/html'});
 	res.write('<h1>hi</h1>');
-	res.end('there');
+	res.end('here');
 }).listen(port);
 

46abbf047df79f96471a1a67b5a9def4c951ebeb
ferrarima <yzxz@hotmail.com>
2015-06-25 10:58:08 +0800

    check

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var port = 18080;
 http.createServer(function (req, res){
-	res.writeHEAD(200, {'Content-type':'text/html'});
+	res.writeHead(200, {'Content-type':'text/html'});
 	res.write('<h1>hi</h1>');
 	res.end('there');
 }).listen(port);
